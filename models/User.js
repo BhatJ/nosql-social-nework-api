0df9b7677d2c8599e8e011d342f76969031f1295
BhatJ <jisha2209@gmail.com>
2023-04-03 08:58:13 +1000

    Created the User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,51 @@
-// Todo - User model
+const { Schema, model } = require('mongoose');
+
+// Schema to create a User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called `friendCount` that retrieves the length of the 
+// user's `friends` array field on query.
+
+userSchema
+  .virtual('friendCount')
+  .get(function() {
+    return this.friends.length;
+  });
+
+// Initialise the User model
+const User = model('user', userSchema);
+
+module.export = User;
